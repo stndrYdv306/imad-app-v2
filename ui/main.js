847a4b8e5c7025a9a61919b67e714f7b7c9ce951
stndrYdv306 <satyendra.1997.yadav@gmail.com>
2017-02-14 21:12:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML= 'INTRODUCING MYSELF IN IMAD 2017 TO LEARN MODERN APPLICATION DEVELOPMENT';
+var img=document.getElementById('madi');
+img.onclick= function()
+{
+    img.style.marginright='100px';
+}
