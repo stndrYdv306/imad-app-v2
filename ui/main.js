d519b71911dce444d39e23e518bc45b4e45bc752
stndrYdv306 <satyendra.1997.yadav@gmail.com>
2017-02-19 20:01:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,15 @@
 var button=document.getElementById('counter');
-var counter = 0;
 button.onclick= function(){
-    counter=counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-
+    var request= new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readystate===XMLHttprequest.DONE){
+        if(request.status===200){
+           var counter=request.responseText;
+           var span=document.getElementById('count');
+           span.innerHTML=counter.toString();
+        }
+    }
+    };
+    request.open('GET','http://stndrydv306.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
