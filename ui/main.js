d6538fa38dadbd303d35990fe3af2e9493ead335
stndrYdv306 <satyendra.1997.yadav@gmail.com>
2017-02-19 15:24:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML= 'INTRODUCING MYSELF IN IMAD 2017 TO LEARN MODERN APPLICATION DEVELOPMENT';
-var img=document.getElementById('madi');
-img.onclick= function(){
-    img.style.marginLeft='100px';
+var button=document.getElementById('count');
+button.onclick= function(){
+    counter=counter + 1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+
 };
