ff5b47a605bce1e0e420de62e0787e9e4bfccbd1
stndrYdv306 <satyendra.1997.yadav@gmail.com>
2017-02-19 15:27:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button=document.getElementById('count');
+var button=document.getElementById('counter');
 button.onclick= function(){
     counter=counter + 1;
     var span=document.getElementById('count');
